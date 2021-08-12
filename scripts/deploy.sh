3409cb65a1ee4f942ef5961c31a0980be5af245c
MariusCSaunders <mchastinsaunders@gmail.com>
2021-08-12 09:05:29 +0000

    MS: Updated Deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+
 rsync -r docker-compose.yaml nginx docker-manager:
+
 ssh docker-manager << EOF
+    sudo usermod -aG docker $USER
+    docker login -u ${DOCKERHUB_USR} -p ${DOCKERHUB_PSW}
     docker stack deploy --compose-file docker-compose.yaml kendama-order
 EOF
