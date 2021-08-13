48185f2c61c99f4023e71f32da4c84d5911dc9aa
Marius Saunders <86304634+MariusCSaunders@users.noreply.github.com>
2021-08-13 16:52:33 +0100

    Undoing Rolling update tests

--- service_4/app.py
+++ service_4/app.py
@@ -8,22 +8,21 @@ app = Flask(__name__)
 
 prices = {
     'damas': {
-        'Krom Beans': 45.99,
-        'SK Mor': 26.99,
-        'Sol Alex M': 44.99,
-        'Sweets Splice': 74.99,
-        'GT TF': 105.00,
-        'Cereal 1.5 SK': 35.99
+        'Krom Gas': 40.00,
+        'SK Lomond': 36.99,
+        'Sol Liam R Mod': 44.99,
+        'Sweets Decade': 74.99,
+        'GT Nic Stodd': 45.00,
+        'Cereal WC Performer': 33.99
     },
     'accessories': {
-        'Go Pro': 234.99,
-        'Tripod': 19.99,
-        'Adobe Premiere Pro': 139.99,
-        'Editing Computer': 1499.99,
-        'RED DSMC2': 14499.99,
-        'Power Bank': 37.99
+        'Dama Holster': 4.99,
+        'T-Shirt': 19.99,
+        'String Pack': 4.99,
+        'Pin Badge': 0.00,
+        '5 Panel Hat': 24.99,
+        'Ken Care Kit': 18.99
     }
-}
 
 @app.route('/post/order', methods=['POST'])
 def post_order():
