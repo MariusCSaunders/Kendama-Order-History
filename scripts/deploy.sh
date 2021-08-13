ac6e8301394cf199bd86b71057420771ce2eb491
MariusCSaunders <mchastinsaunders@gmail.com>
2021-08-13 08:17:16 +0000

    MS: Updated files for jenkins credentials

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,8 @@ rsync -r docker-compose.yaml nginx docker-manager:
 
 ssh docker-manager << EOF
     sudo usermod -aG docker $USER
+    export DATABASE_URI=${DATABASE_URI}
+    export DOCKERHUB=${DOCKERHUB}
     docker login -u ${DOCKERHUB_USR} -p ${DOCKERHUB_PSW}
     docker stack deploy --compose-file docker-compose.yaml kendama-order
 EOF
