4ad41e46f4dc2fd9528d96008523bd2823be3cd8
MariusCSaunders <mchastinsaunders@gmail.com>
2021-08-14 11:44:01 +0000

    MS: Json error debugging

--- service_1/application/routes.py
+++ service_1/application/routes.py
@@ -5,6 +5,7 @@
 from flask import render_template
 import requests
 
+
 from . import app, db
 from .models import Orders
 
@@ -12,6 +13,8 @@ from .models import Orders
 def home():
     dama = requests.get('http://service-2:5000/get/dama').text
     accessory = requests.get('http://service-3:5000/get/accessories').text
+
+    print(dama, accessory)
 
     price_request = {'damas': dama, 'accessories': accessory}
     price = requests.post('http://service-4:5000/post/order', json=price_request).json()
@@ -22,4 +25,8 @@ def home():
 
     order_history = Orders.query.order_by(Orders.id.desc()).limit(5).all()
         
-    return render_template("home.html", orders=order_history)
+    return render_template("home.html", orders=order_history, current_order=order)
+
+
+
+
